7d75be4d0c61fd686b089c61a5336b2041cf07f8
amanjuneja420 <amanj2002@gmail.com>
2022-10-06 01:40:47 +0530

    added code to calculate area and perimeter of circle of given radius in python

---
+++ 33. Python Programs/areaandperimeterofcircle.py
@@ -0,0 +1,6 @@
+def areaandperimeterofcircle():
+    r=int(input("enter the radius of circle : "))
+    print("area = ",3.14159*r*r)
+    print("perimeter = ",3.14159*2*r)
+
+areaandperimeterofcircle()
