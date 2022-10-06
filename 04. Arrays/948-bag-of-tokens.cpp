b3ed659ba30e8ddf28bf7186294c8023adcef5e4
Aastha Dubey <aasthad27@gmail.com>
2022-10-06 17:14:13 +0530

    Update 948-bag-of-tokens.cpp

--- 04. Arrays/948-bag-of-tokens.cpp
+++ 04. Arrays/948-bag-of-tokens.cpp
@@ -33,32 +33,4 @@ Constraints:
 0 <= tokens.length <= 1000
 0 <= tokens[i], power < 104 ***
 
-``` code ```
-class Solution {
-public:
-    int bagOfTokensScore(vector<int>& tokens, int power) {
-        sort(tokens.begin(),tokens.end());
-        int n=tokens.size();
-        int l=0;
-        int r=tokens.size();
-        while(l<r)
-        {
-           if(power>=tokens[l])  // face up 
-           {
-               power=power-tokens[l];
-               l++;
-           }
-            else if(l-(n-r) && r>l+1) // face down
-            {
-                                r--;
 
-                power=power+tokens[r];
-            }
-            else
-            {
-                break;
-}
-}
-        return l-(n-r);
-    }
-};
