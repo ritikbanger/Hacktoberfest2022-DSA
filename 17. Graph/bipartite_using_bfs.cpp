ef73941d80c7a42912fa8a54d0f1f8059dafdf7e
Deepak-technical <deepakakash156@gmail.com>
2022-10-07 17:08:56 +0530

    Question addded

---
+++ 17. Graph/bipartite_using_bfs.cpp
@@ -0,0 +1,75 @@
+// A Bipartite Graph is a graph whose vertices can be divided into two independent sets, U and V such that every edge (u, v) either connects a vertex from U to V or a vertex from V to U. 
+// In other words, for every edge (u, v), either u belongs to U and v to V, or u belongs to V and v to U. We can also say that there is no edge that connects vertices of same set.
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+private:
+    bool check(int start, int V, vector<int> adj[], int color[])
+    {
+        // Code here
+        queue<int> q;
+        q.push(start);
+
+        color[start] = 0;
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            for (auto it : adj[node])
+            {
+                if (color[it] == -1)
+                {
+                    color[it] = !color[node];
+                    q.push(it);
+                }
+                else if (color[it] == color[node])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+public:
+    bool isBipartite(int V, vector<int> adj[])
+    {
+        int color[V];
+        for (int i = 0; i < V; i++)
+            color[i] = -1;
+        for (int i = 0; i < V; i++)
+        {
+            if (color[i] == -1)
+            {
+                if (check(i, V, adj, color) == false)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+int main()
+{
+    int V, E;
+    cin >> V >> E
+                    vecyor<int>
+                        adj[V];
+    for (int i = 0; i < E; i++)
+    {
+        int u, v;
+        cin u, v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    Solution obj;
+    bool ans = obj.isBipartite(V, adj);
+    if (ans)
+        cout << "Yess It a Bipartite Graph";
+    else
+        cout << "It Is not a bipartite Graph";
+}
