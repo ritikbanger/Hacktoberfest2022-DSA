8beb5f7e523e941783df8ed465800facf41f5faf
Ayush Agrawal <56473603+AyushAgrawal25@users.noreply.github.com>
2022-10-08 09:49:39 +0530

    Update Morris_Traversal_on_Binary_Tree.cpp
    Now it will take dynamic Inputs.
    
    Input format : 
    N is the number of nodes.
    Arr is the array of nodes.
    -1 is the NULL node.

--- 14. Tree/Morris_Traversal_on_Binary_Tree.cpp
+++ 14. Tree/Morris_Traversal_on_Binary_Tree.cpp
@@ -1,65 +1,102 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node {
-	public:
-  int data;
-	struct Node* left;
-	struct Node* right;
+class Node
+{
+public:
+    int data;
+    struct Node *left;
+    struct Node *right;
 };
 
-void MorrisTraversal(struct Node* root)
+void MorrisTraversal(struct Node *root)
 {
-	struct Node *current, *pre;
+    struct Node *current, *pre;
 
-	if (root == NULL)
-		return;
+    if (root == NULL)
+        return;
 
-	current = root;
-	while (current != NULL) {
+    current = root;
+    while (current != NULL)
+    {
 
-		if (current->left == NULL) {
-			cout << current->data << " ";
-			current = current->right;
-		}
-		else {
-			pre = current->left;
-			while (pre->right != NULL
-				&& pre->right != current)
-				pre = pre->right;
+        if (current->left == NULL)
+        {
+            cout << current->data << " ";
+            current = current->right;
+        }
+        else
+        {
+            pre = current->left;
+            while (pre->right != NULL && pre->right != current)
+                pre = pre->right;
 
-			if (pre->right == NULL) {
-				pre->right = current;
-				current = current->left;
-			}
-			else {
-				pre->right = NULL;
-				cout << current->data << " ";
-				current = current->right;
-			} 
-		} 
-	} 
+            if (pre->right == NULL)
+            {
+                pre->right = current;
+                current = current->left;
+            }
+            else
+            {
+                pre->right = NULL;
+                cout << current->data << " ";
+                current = current->right;
+            }
+        }
+    }
 }
 
-struct Node* newNode(int data)
+struct Node *newNode(int data)
 {
-	struct Node* node = new Node;
-	node->data = data;
-	node->left = NULL;
-	node->right = NULL;
+    struct Node *node = new Node;
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
 
-	return (node);
+    return (node);
 }
 
 int main()
-{ 
-  struct Node* root = newNode(1);
-	root->left = newNode(2);
-	root->right = newNode(3);
-	root->left->left = newNode(4);
-	root->left->right = newNode(5);
+{
+    // Take dynamic input for binary tree.
+    int n;
+    cin >> n;
 
-	MorrisTraversal(root);
+    vector<int> arr(n, 0);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
 
-	return 0;
+    struct Node *root = newNode(arr[0]);
+    queue<struct Node *> q;
+    q.push(root);
+
+    int i = 1;
+    while (!q.empty() && i < n)
+    {
+        struct Node *temp = q.front();
+        q.pop();
+
+        if (arr[i] != -1)
+        {
+            temp->left = newNode(arr[i]);
+            q.push(temp->left);
+        }
+        i++;
+
+        if (i >= n)
+            break;
+
+        if (arr[i] != -1)
+        {
+            temp->right = newNode(arr[i]);
+            q.push(temp->right);
+        }
+        i++;
+    }
+
+    MorrisTraversal(root);
+
+    return 0;
 }
