984ed8aac9bb0f5d90d8196a92388e502914200d
itsparth47 <93420021+itsparth47@users.noreply.github.com>
2022-10-12 11:00:57 +0530

    Create Valid Anagram.cpp

---
+++ 27. LeetCode Problems/Valid Anagram.cpp
@@ -0,0 +1,13 @@
+//valid anagram leetcode solution
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(s==t)
+        return true;
+        else
+            return false;
+    }
+};
