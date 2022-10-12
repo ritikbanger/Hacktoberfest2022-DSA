f4531c29eec5fc6b9cfd4ec2f219b49a3b1ae792
Aman Kumar <56875750+amankrbes3@users.noreply.github.com>
2022-10-12 09:22:47 +0530

    Update 20_gcd.cpp

--- 01. Mathematics/20_gcd.cpp
+++ 01. Mathematics/20_gcd.cpp
@@ -6,11 +6,22 @@
  #define vl vector<ll>
  #define pii pair<int,int>
  #define pll pair<ll,ll>
-
+ int static dp[1001][1001];
   int gcd(int a,int b)
  {
      if(b==0){return a;}
      else {return gcd(b,b%a);}
+ }
+ 
+//program for gcd of two number using dynamic programming
+ int get_gcd(int a,int b){
+    if (a == 0) return b;
+    if (b == 0) return a;
+    if (a == b) return a;
+    if(dp[a][b] != -1) return dp[a][b];
+    if (a > b) dp[a][b] = gcd(a-b, b);
+    else dp[a][b] = gcd(a, b-a);
+    return dp[a][b];
  }
  int main()
  {
@@ -22,7 +33,7 @@
      cout<<"Enter two num of which gcd you want to find\n";
      int x,y;
      cin>>x>>y;
-     cout<<gcd(x,y);
+     cout<<get_gcd(x,y);
   
  return 0;
  }
