7162d1eb219f2e8e2f5d3d50888dfcd6c1024000
Mohan0006 <87588310+Mohan0006@users.noreply.github.com>
2022-10-14 19:09:05 +0530

    Solution to LeetCode Problem Number Of Islands

---
+++ 27. LeetCode Problems/200. Number of Islands.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    
+    
+    void checkIslands(vector<vector<char>>&grid,int i,int j,int m,int n){
+        
+        if(i<0||j<0||i>=m||j>=n||grid[i][j]!='1'){
+            return;
+        }
+        grid[i][j] = '2';
+        checkIslands(grid,i-1,j,m,n);
+        checkIslands(grid,i,j-1,m,n);
+        checkIslands(grid,i+1,j,m,n);
+        checkIslands(grid,i,j+1,m,n);
+
+        
+    }
+    
+    
+    
+    int countIslands(vector<vector<char>> &grid){
+        
+        int m = grid.size();
+        int n = grid[0].size();
+        int count  = 0;
+        for(int i = 0 ; i < m ;i++){
+            for(int j = 0 ;j < n ;j++){
+                  if(grid[i][j]=='1'){
+                      count++;
+                      checkIslands(grid,i,j,m,n);
+                  }        
+            }
+        }
+        
+        return count;
+        
+        
+    }
+    
+    
+    int numIslands(vector<vector<char>>& grid) {
+        return countIslands(grid);
+    }
+};
