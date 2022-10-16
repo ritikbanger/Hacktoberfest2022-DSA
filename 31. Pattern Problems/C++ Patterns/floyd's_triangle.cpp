be8d1a58e4511f7ca7593c3ca9825c55c20ba5d1
Saurav Kumar <sauravkumar843114@gmail.com>
2022-10-16 22:01:50 +0530

    Here I have contributed some examples of DSA and some corrections to remove errors.

--- 31. Pattern Problems/C++ Patterns/floyd's_triangle.cpp
+++ 31. Pattern Problems/C++ Patterns/floyd's_triangle.cpp
@@ -2,17 +2,32 @@
 2 3
 4 5 6
 7 8 9 10*/
-#include<stdio.h>
-int main(){
-    int i, j, space, value=1,no=1;
-    for(i=1;i<=10;++i)
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the number of row : ";
+    cin >> n;
+
+    int i = 1;
+
+    while (i <= n)
     {
-        for(j=1;j<=i;j++)
+        int j = 1;
+        int value = i;
+        while (j <= i)
         {
-            printf("%d ",no);
-            no++;
+            cout << value<<" ";
+            value = value + 1;
+            j = j + 1;
         }
-        printf("\n");
+        cout << endl;
+        i = i + 1;
     }
     return 0;
 }
+
+/* I think my way of approch is better than previous one, it is more clear and easily understandable :)*/
