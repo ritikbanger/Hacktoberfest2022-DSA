e86adb28ea82590bcc9c1a3bb06995ce6525f347
ayush91985 <67649316+ayush91985@users.noreply.github.com>
2022-10-19 21:25:48 +0530

    added main function with syntax.

--- 30. CPP Programs/1. Two sums.cpp
+++ 30. CPP Programs/1. Two sums.cpp
@@ -1,21 +1,43 @@
-class Solution
-{
+//ayush srivastava
+//github- ayush91985
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
 public:
-    vector<int> twoSum(vector<int> &nums, int target)
-    {
-        vector<int> ans = {};
-        unordered_map<int, int> hashMap;
-        for (int i = 0; i < nums.size(); i++)
-        {
-            if (hashMap.find(target - nums[i]) != hashMap.end())
-            {
-                ans.push_back(hashMap[target - nums[i]]);
-                ans.push_back(i);
-                return ans;
-            }
-            hashMap[nums[i]] = i;
-        }
-        return ans;
-    }
+    
+      vector<int> twoSum(vector<int>& nums, int target) {
+                map<int,int> m;
+                vector<int> v;
+                int n= nums.size();
+                for(int i=0;i<n;i++)
+                {
+                    
+                        int diff = target - nums[i];
+                        if(m.find(diff) != m.end())
+                        {
+                        auto p = m.find(diff);        
+                        v.push_back(p->second);
+                        v.push_back(i);
+                        }
+                        m.insert(make_pair(nums[i],i));
+                }
+          
+                return v;
+}
 };
-// https://leetcode.com/problems/two-sum/discuss/?currentPage=1&orderBy=hot&query=
+
+
+int main(){
+    Solution s ;
+    vector<int> v;
+     v={1,6,3,2,5};
+     vector<int> result= s.twoSum (v, 11);
+     for(int i: result)
+     {
+          cout<<i<<endl;
+     }
+     return 0;
+
+}
