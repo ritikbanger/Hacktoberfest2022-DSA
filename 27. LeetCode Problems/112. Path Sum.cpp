724c25d4ce9819e4c328c25f2ea5f41b33f1fd6a
ketan jangid <ketanjangid786@gmail.com>
2022-10-23 01:38:00 +0530

    Add 112. Path Sum in LeetCode Problems

---
+++ 27. LeetCode Problems/112. Path Sum.cpp
@@ -0,0 +1,30 @@
+/*
+
+Link to the Problem - https://leetcode.com/problems/path-sum/
+
+Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+*/
+
+
+//using recursion
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(!root)   return false;
+
+        targetSum -= root->val;
+
+        if(!root->left && !root->right && targetSum==0)  return true;
+
+        bool left = false;
+        if(root->left)
+            left = hasPathSum(root->left, targetSum);
+
+        bool right = false;
+        if(root->right)
+            right = hasPathSum(root->right, targetSum);
+
+        return left|right;
+    }
+};
