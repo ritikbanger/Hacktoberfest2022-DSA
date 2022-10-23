20189b0c49085f847fb66b21660b2718e4fed129
Sameeksha Mishra <sameeksha.mishra94@gmail.com>
2022-10-23 23:01:29 +0530

    Modified NumberofIslands.cpp

--- 17. Graph/NumberofIslands.cpp
+++ 17. Graph/NumberofIslands.cpp
@@ -1,42 +1,43 @@
-//Leetcode Medium Level
+// Leetcode Medium Level
 
-//Problem link : https://leetcode.com/problems/number-of-islands/
+// Problem link : https://leetcode.com/problems/number-of-islands/
 
+#include <bits/stdc++.h>
+using namespace std;
 
-class Solution {
-    void mark_cur_island(vector<vector<char>> &matrix, int x, int y, int r, int c){
-        
-        //Corner Cases for matrix
-        if(x<0 || x >=r || y<0 || y>=c || matrix[x][y]!='1'){
-            return;
+class Solution
+{
+    int dx[4] = {1, -1, 0, 0};
+    int dy[4] = {0, 0, -1, 1};
+    void dfs(int i, int j, vector<vector<char>> &grid, int n, int m)
+    {
+        grid[i][j] = '2';
+        for (int k = 0; k < 4; k++)
+        {
+            int nexti = i + dx[k];
+            int nextj = j + dy[k];
+            if (nexti >= 0 && nexti < n && nextj >= 0 && nextj < m && grid[nexti][nextj] == '1')
+            {
+                dfs(nexti, nextj, grid, n, m);
+            }
         }
-        
-        //Consider this cell as visited
-        matrix[x][y] = '2';
-        
-        mark_cur_island(matrix,x+1,y,r,c); //DOWN
-        mark_cur_island(matrix,x-1,y,r,c); //TOP
-        mark_cur_island(matrix,x,y+1,r,c); //RIGHT
-        mark_cur_island(matrix,x,y-1,r,c); //LEFT
     }
+
 public:
-    int numIslands(vector<vector<char>>& grid) {
-        
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        int rows = grid.size();
-        if(rows==0)
-            return 0; //Empty grid, return zero island
-        int columns = grid[0].size();
-        int islands = 0;
-        for(int i=0; i<rows; i++){
-            for(int j=0; j<columns; j++){
-                if(grid[i][j]=='1'){
-                    mark_cur_island(grid,i,j,rows,columns);
-                    islands += 1;
+    int numIslands(vector<vector<char>> &grid)
+    {
+        int cnt = 0, n = grid.size(), m = grid[0].size();
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (grid[i][j] == '1')
+                {
+                    dfs(i, j, grid, n, m);
+                    cnt++;
                 }
             }
         }
-        return islands;
+        return cnt;
     }
 };
