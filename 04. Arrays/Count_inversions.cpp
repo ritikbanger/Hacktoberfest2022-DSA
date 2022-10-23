16cc1929ebfaed29e6db3189721eb717849a0146
Sameeksha Mishra <sameeksha.mishra94@gmail.com>
2022-10-23 23:52:18 +0530

    Modified Count_inversions.cpp

--- 04. Arrays/Count_inversions.cpp
+++ 04. Arrays/Count_inversions.cpp
@@ -1,47 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
-long long int merger(vector<long long> &v, int si, int ei, int mid) {
-    int ls = si, le = mid;
-    int rs = mid + 1, re = ei;
-    long long temp[ei - si + 1];
-    int k = 0;
-    long long cnt = 0;
-    while (ls <= le && rs <= re) {
-        if (v[ls] > v[rs]) {
-            temp[k++] = v[rs++];
-            cnt += le - ls + 1;
-        } else if (v[ls] <= v[rs]) {
-            temp[k++] = v[ls++];
+long long int merge(int arr[], int temp[], int left, int mid, int right)
+{
+    int inv_count = 0;
+    int i = left;
+    int j = mid;
+    int k = left;
+    while ((i <= mid - 1) && (j <= right))
+    {
+        if (arr[i] <= arr[j])
+        {
+            temp[k++] = arr[i++];
+        }
+        else
+        {
+            temp[k++] = arr[j++];
+            inv_count = inv_count + (mid - i);
         }
     }
-    while (ls <= le) {
-        temp[k++] = v[ls++];
-    }
-    while (rs <= re) {
-        temp[k++] = v[rs++];
-    }
-    k = 0;
-    for (int i = si; i < ei + 1; i++) {
-        v[i] = temp[k++];
-    }
-    return cnt;
+
+    while (i <= mid - 1)
+        temp[k++] = arr[i++];
+
+    while (j <= right)
+        temp[k++] = arr[j++];
+
+    for (i = left; i <= right; i++)
+        arr[i] = temp[i];
+
+    return inv_count;
 }
-long long int helper(vector<long long> &v, int si, int ei) {
-    if (si >= ei) {
-        return 0;
-    }
-    int mid = (si + ei) / 2;
-    long long left = helper(v, si, mid);
-    long long right = helper(v, mid + 1, ei);
-    return (left + right + merger(v, si, ei, mid));
-}
+
 long long int inversionCount(vector<long long> arr, long long N) {
     // Your Code Here
-    vector<long long> v;
-    for (int i = 0; i < N; i++) {
-        v.push_back(arr[i]);
+    int mid, inv_count = 0;
+    if (right > left)
+    {
+        mid = (left + right) / 2;
+
+        inv_count += inversionCount(arr, temp, left, mid);
+        inv_count += inversionCount(arr, temp, mid + 1, right);
+
+        inv_count += merge(arr, temp, left, mid + 1, right);
     }
-    return helper(v, 0, N - 1);
+    return inv_count;
 }
 int main() {
     long long n;
