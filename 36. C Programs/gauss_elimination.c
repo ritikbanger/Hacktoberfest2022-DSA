20e871bd9d1b4687eaab7e230e39eb1d03b97a02
Ishika Gupta <88900614+ishika-gupta21@users.noreply.github.com>
2022-10-30 22:27:58 +0530

    Gauss Elimination of solution to equations

---
+++ 36. C Programs/gauss_elimination.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <conio.h>
+#include <Math.h>
+float matrix[10][10], m, temp[10];
+int i, j, k, n;
+void lower_traingularisation() 
+{
+    for (i=n-1; i>0; i--)
+    {
+        for (j=i-1; j>=0; j--) 
+        {
+            m= matrix[j][i]/matrix[i][i];
+            for (k=0; k<n+1; k++) 
+            {
+                matrix[j][k] = matrix[j][k]-(m*matrix[i][k]);
+            }
+        }
+    }
+}
+void back_subsitution()
+{
+    for (i=0; i<n; i++)
+    {
+        m = matrix[i][n];
+        for (j=0; j<i; j++)
+            m = m - temp[j] * matrix[i][j];
+        temp[i] = m/matrix[i][i];
+        printf("\n x%d => %f", i+1, temp[i]);
+    }
+}
+ 
+void main() 
+{
+    printf("Name: Ishika Gupta\nUniversity Roll No.: 2016786\nSemester: 5\nSection/Class Roll No. :B-48\n");
+    printf("Enter the order of the matrix: ");
+    scanf("%d", &n);
+    printf("Enter the augmented matrix: \n");
+    for (i=0; i<n; i++)
+    {
+        for (j=0; j<n+1; j++)
+        {
+            printf("a[%d][%d] = ",i,j);
+            scanf("%f", &matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    lower_traingularisation();
+    printf("The lower traingular matrix is : \n");
+    for (i=0; i<n; i++) 
+    {
+        for (j=0; j<n+1; j++)
+            printf("%f \t", matrix[i][j]);
+        printf("\n");
+    }
+    printf("\nThe required result is : \n");
+    back_subsitution();
+    getch();
+} 
