13cd8c05d35eebf974d545944205e8079e56ebc0
Rina Kushwaha <119058627+itiansrina@users.noreply.github.com>
2023-10-05 23:12:38 +0530

    Update fibbonacci.java

--- 32. Java Programs/fibbonacci.java
+++ 32. Java Programs/fibbonacci.java
@@ -33,3 +33,18 @@ class BIG {
 		Fibonacci(N);
 	}
 }
+
+leetcode 
+class Solution {
+    public int fib(int n) {
+        if(n==0||n==1)
+        {
+            return n;
+        }
+        int fib1=fib(n-1);
+        int fib2=fib(n-2);
+        int ans=fib1+fib2;
+        return ans;
+        
+    }
+}
