c838947519c2c61d55acd009d84cb2fe4b7a2385
Rina Kushwaha <119058627+itiansrina@users.noreply.github.com>
2023-10-12 12:39:04 +0530

    Update Palindrome.java (#488)

--- 32. Java Programs/Palindrome.java
+++ 32. Java Programs/Palindrome.java
@@ -14,3 +14,24 @@ class example{
    System.out.println("not palindrome");    
 }  
 }  
+
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        int temp=x;
+        int rev=0;
+
+        while(x>0)
+        {
+           int rem=x%10;
+           rev=(rev*10)+rem;
+            x=x/10;
+        }
+        if(rev==temp)
+           return true;
+        else 
+           return false;
+
+        
+    }
+}
